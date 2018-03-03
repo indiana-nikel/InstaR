da517189fc1f563963bae6eb4eb4f230baae06ff
Tarini Bhatnagar <tarini.bhatnagar@gmail.com>
2018-03-03 15:17:52 -0800

    added R file for flip function

---
+++ R/flip.R
@@ -0,0 +1,13 @@
+#Copyright 2018 Tarini Bhatnagar
+#Licensed under the Apache License, Version 2.0 (the "License")
+#you may not use this file except in compliance with the License.
+#You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+# March 2018
+# This script the function for flip.R.
+
+library(InstaR)
+
+flip <- function(X, direction) {
+  return(NULL)
+}
